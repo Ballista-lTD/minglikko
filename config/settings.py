d779563d1e73d7c87b4d0b48edab30c62a8f5d5f
sunith <sunithvs2002@gmail.com>
2022-02-10 02:46:24 +0530

    migration ayi

--- config/settings.py
+++ config/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'request_viewer.middleware.RequestViewerMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
