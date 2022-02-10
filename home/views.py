0a4c45957b997eaede5683359e730d8b56ecd6e1
sunith <sunithvs2002@gmail.com>
2022-02-10 14:27:23 +0530

    working now style

--- home/views.py
+++ home/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 rules = ["maryadak ullathu ullath pole parayanam", "maryadak ullathu ullath pole parayanam", 'kooooooooooooooi']
 
+questions = ['ningal aranu', 'ningalk enth venam', 'irangi podo']
+
 
 @login_required(login_url='/')
 def index(request):
-    context1 = {'rules': rules}
+    context1 = {'rules': rules, 'questions': questions}
     return render(request, template_name='index.html', context=context1)
--- static/js/main.js
+++ static/js/main.js
@@ -43,36 +43,10 @@
     /**
      * Back to top button
      */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-        const toggleBacktotop = () => {
-            if (window.scrollY > 100) {
-                backtotop.classList.add('active')
-            } else {
-                backtotop.classList.remove('active')
-            }
-        }
-        window.addEventListener('load', toggleBacktotop)
-        onscroll(document, toggleBacktotop)
-    }
 
     /**
      * Countdown timer
      */
-    let countdown = select('.countdown');
-    const output = countdown.innerHTML;
 
-    const countDownDate = function () {
-        let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
-
-        let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-        countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
-    }
-    countDownDate();
-    setInterval(countDownDate, 1000);
 
 })()
