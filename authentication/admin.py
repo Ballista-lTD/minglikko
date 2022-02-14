47120b50af158da5b65fa5db91b34d3ab2996be7
sunith <sunithvs2002@gmail.com>
2022-02-14 06:06:30 +0530

    changed data

--- authentication/admin.py
+++ authentication/admin.py
@@ -7,7 +7,7 @@ from .models import Tokens
 @admin.register(Tokens)
 class TokenAdmin(admin.ModelAdmin):
     list_display = ['name', 'user', 'total']
-    search_fields = ['name', 'user', ]
+    search_fields = ['name', ]
 
 
 
