e1fbee72c06e7cbe1820b2ae92c3fea7a9610bf3
sunith <sunithvs2002@gmail.com>
2022-02-16 01:14:06 +0530

    working on chats

--- authentication/admin.py
+++ authentication/admin.py
@@ -7,7 +7,7 @@ from .models import Tokens
 @admin.register(Tokens)
 class TokenAdmin(admin.ModelAdmin):
     list_display = ['name', 'user', 'total']
-    search_fields = ['name', 'user']
+    search_fields = ['name', ]
     # actions = ['set_chat_friend']
     #
     # def set_chat_friend(self, tkn):
