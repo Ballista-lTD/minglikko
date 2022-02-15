6b9ce36ac04e754a8d98ae2fb2f3448b1aecdc37
sunith <sunithvs2002@gmail.com>
2022-02-15 18:56:36 +0530

    ellam work ayach makkale

--- home/algorithm.py
+++ home/algorithm.py
@@ -9,7 +9,6 @@ final = [tk.name for tk in Tokens.objects.exclude(total=0)]
 
 
 def give_random_priority():
-    print(f"{total = }")
     tkns = Tokens.objects.filter().exclude(total=0)
     print(tkns.count())
     count = 0
@@ -22,7 +21,6 @@ def give_random_priority():
         tkn.priority_list.extend(ls)
         tkn.save()
         print(len(tkn.priority_list), len(ls))
-    print(f"{count = }")
 
 
 def print_stats():
@@ -37,16 +35,16 @@ def print_stats():
     pprint(data)
 
 
-def create_list(ls=1):
+def create_list():
     tkns = Tokens.objects.all().exclude(total=0)
     applications = {}
     for tkn in tkns.all():
         prio = {}
         for i in range(len(tkn.priority_list)):
-            prio[tkn.priority_list[i]] = i + 1
+            prio[tkn.priority_list[i]] = i
 
-        applications[tkn.name] = {"prio_list": tkn.priority_list if ls else prio, "accepted": False,
-                                  "accepted_from": ''}
+        applications[tkn.name] = {"prio_dict": prio, "accepted": False,
+                                  "accepted_from": None, "prio_list": tkn.priority_list, "proposals": []}
 
     return applications
 
@@ -65,9 +63,7 @@ def validate_data(users: dict):
 
 
 def it():
-    users = create_list(1)
-    clone = create_list(2)
-
+    users = create_list()
     posts: dict[list] = {}
 
     for user in users:
@@ -78,37 +74,57 @@ def it():
         stop = True
         for user in users:
 
-            user_data = users[user]
-            if not user_data['accepted']:
-                next_user = user_data['prio_list'].pop(0)
-                next_user_info = clone[next_user]
-                if not next_user_info['accepted']:
-                    clone[next_user]['accepted_from'] = user
-                    users[user]['accepted'] = True
-                    users[next_user]['accepted'] = True
-                else:
-                    old_user = next_user_info['accepted_from']
-                    old_user_prio = next_user_info['prio_list'][old_user]
-                    new_user_prio = next_user_info['prio_list'][user]
-                    if new_user_prio < old_user_prio:
-                        clone[next_user]['accepted_from'] = user
-                        users[user]['accepted'] = True
-                        users[next_user]['accepted'] = True
-                        users[old_user]['accepted'] = False
-                        clone[old_user]['accepted'] = False
-
+            sender = users[user]
+            if not sender['accepted_from'] and len(sender['prio_list']):
+                print(len(sender['prio_list']), end='\n\n')
+                users[sender['prio_list'].pop()]['proposals'].append(user)
                 stop = False
-    return clone
+
+        for user in users:
+            receiver = users[user]
+
+            if len(receiver['proposals']):
+                rank, the_name = -1, ""
+
+                for name in receiver['proposals']:
+                    if receiver['prio_dict'][name] > rank:
+                        rank = receiver['prio_dict'][name]
+                        print(the_name)
+                        the_name = name
+
+                receiver['proposals'] = []
+
+                if receiver['accepted_from']:
+                    if receiver['prio_dict'][receiver['accepted_from']] > rank:
+                        break_up = receiver['accepted_from']
+                        receiver['accepted_from'] = the_name
+                        users[break_up]['accepted_from'] = None
+                    else:
+                        pass
+                else:
+                    print(the_name)
+                    receiver['accepted_from'] = the_name
+
+    return users
+
+
+def clear_partners():
+    for tkn in Tokens.objects.all():
+        tkn.partner = None
+        tkn.save()
 
 
 def perform_algorithm():
-    give_random_priority()
+    # give_random_priority()
+    clear_partners()
     result = it()
     data = {}
 
+    # print(result)
+
     for res in result:
         data[res] = result[res]['accepted_from']
-
+    print(data)
     count_invalid = 0
     count_empty = 0
     lst = []
@@ -119,12 +135,17 @@ def perform_algorithm():
             lst.extend([dt, data[dt]])
     final_list = {}
     for dt in data:
-        if data[dt] != '':
+        if data[dt]:
+
             final_list[dt] = data[dt]
 
     for tkn in final_list:
+        print(tkn)
         token = Tokens.objects.get(name=tkn)
         token.set_partner(final_list[tkn])
 
-    print(f"Total valid pairs {len(lst)}\n invalid pairs = {count_invalid} \n{count_empty =} ")
+    print(
+        f"Total valids {Tokens.objects.exclude(partner=None).count()}\n invalid pairs = {count_invalid} \n{count_empty =} ")
 
+
+perform_algorithm()
