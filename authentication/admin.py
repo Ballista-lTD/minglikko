214142bae92f818f9aeb545e299a499dc5a5aa70
sunith <sunithvs2002@gmail.com>
2022-02-16 11:52:05 +0530

    fixing algos

--- authentication/admin.py
+++ authentication/admin.py
@@ -6,7 +6,7 @@ from .models import Tokens
 
 @admin.register(Tokens)
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ['name', 'user', 'total', ]
+    list_display = ['name', 'user', 'total', 'friend']
     search_fields = ['name', ]
     list_per_page = 600
 
@@ -15,9 +15,9 @@ class TokenAdmin(admin.ModelAdmin):
     # def set_chat_friend(self, tkn):
     #     admin_tkn = Tokens.objects.get(user__email='admin@trebuchet.one')
     #     admin_tkn.chat_friends.add(tkn)tk
-    # def friend(self, request):
-    #     return "\n".join([f"{tkn.user.username}: o-{tkn.priority_list.index(request.name)}" for tkn in
-    #                       request.chat_friends.all()])
+    def friend(self, request):
+        return "\n".join([f"{tkn.user.username}: o-{tkn.priority_list.index(request.name)}" for tkn in
+                          request.chat_friends.all()])
     # #
     # def get_queryset(self, request):
     #     return Tokens.objects.all().exclude(total=0)
